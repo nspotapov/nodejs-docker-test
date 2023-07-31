06487ab4e8831cf081367761432c0a526dbc4a3e
ns.potapov <ns.potapov@yandex.ru>
2023-07-31 17:35:11 +0400

    ulstutimebot promo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router();
 
 const path = __dirname + "/views/";
-const port = 8080;
+const port = 8097;
 
 router.use(function (req, res, next) {
 	console.log("/" + req.method);
@@ -14,13 +14,9 @@ router.get("/", function (req, res) {
 	res.sendFile(path + "index.html");
 });
 
-router.get("/sharks", function (req, res) {
-	res.sendFile(path + "sharks.html");
-});
-
 app.use(express.static(path));
 app.use("/", router);
 
 app.listen(port, function () {
-	console.log("Example app listening on port 8080!");
+	console.log("app listening on port " + port);
 });
